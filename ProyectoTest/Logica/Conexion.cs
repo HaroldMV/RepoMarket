887468153c056b2d9dac41a2dd565e99bda39b34
Haritolx2 <harold.mv@outlook.com>
2021-11-22 23:41:39 -0500

    Se agrega el proyecto

---
+++ ProyectoTest/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=.;Initial Catalog=MARKETPLACE_BD_PROY;Integrated Security=True";
+    }
+}
